99314a11be9556c7d4625a63f51899d5d19b8bd4
Fawwaz Atha Rohmatullah <132468764+fawazatha@users.noreply.github.com>
2025-10-28 09:55:14 +0700

    update to add log

--- corporate_action_scraper.py
+++ corporate_action_scraper.py
@@ -10,14 +10,20 @@ import os
 import logging
 
 
-logging.basicConfig(
-    filename='scraper.log', 
-    level=logging.INFO, 
-    format='%(asctime)s [%(levelname)s] - %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S'
-    )
-
 LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.INFO)
+
+file_handler = logging.FileHandler('scraper.log')
+file_handler.setLevel(logging.INFO)
+
+formatter = logging.Formatter(
+    '%(asctime)s [%(levelname)s] - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+file_handler.setFormatter(formatter)
+
+LOGGER.addHandler(file_handler)
+
 LOGGER.info("Init Global Variable")
 
 
